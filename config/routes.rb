8e1cf51b566563eddc5863d9e3faba1d6d84c368
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-12 19:39:19 +0200

    Add about page with get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root "pages#home"
+
+  get "/about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
