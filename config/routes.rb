7914a99a4ced65162cda7717134eda59025da3cb
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-12 22:27:59 +0200

    Add index action resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "pages#home"
 
   get "/about", to: "pages#about"
+
+  resources :articles, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
