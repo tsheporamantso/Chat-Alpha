0cf68abaf048b5b8b7ed97c84790aa667d9cc7d7
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-13 09:19:42 +0200

    Define edit and create methods to update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def article_params
