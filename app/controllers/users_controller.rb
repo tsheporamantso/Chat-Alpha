0328db6aff31221424abedec92b0fa2b20f4885c
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-20 19:55:39 +0200

    Define create action that instatiate a new user if saved correctly user will be redirected to articles index page else render 422 error code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
+
+  def create 
+    @user = User.new( user_params )
+
+    if @user.save
+      flash[:success] = "Welcome to Chat-Alpha, you have successfully signed up."
+      redirect_to articles_path 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
