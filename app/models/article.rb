5498b9225005a9f1ce969649978ef432a7320083
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-20 11:47:27 +0200

    Add associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { in: 6..100 }
   validates :description, presence: true, length: { in: 10..300 } 
 end
