1c156ac2d25b39ef85e995c6be07980341140213
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-20 20:52:18 +0200

    Interpolate username on the flash success message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new( user_params )
 
     if @user.save
-      flash[:success] = "Welcome to Chat-Alpha, you have successfully signed up."
+      flash[:success] = "Welcome to Chat-Alpha #{@user.username}, you have successfully signed up."
       redirect_to articles_path 
     else
       render :new, status: :unprocessable_entity
