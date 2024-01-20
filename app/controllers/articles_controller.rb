5246cf3c50a217359fa0f769314c42c8ffe6032f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-20 11:56:08 +0200

    Add temporary solution to be able to create article and edit existing articles with user_id error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     if @article.save
       flash[:notice] = "Article was successfully created"
