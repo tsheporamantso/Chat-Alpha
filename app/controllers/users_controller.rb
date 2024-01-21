b1b39c717b142fc4f0029ba8bb71fd3cfa0ab42a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-21 18:03:28 +0200

    Define edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:sucess] = "Chat-Alpha user #{ @user.username }, has been updated sucessfully"
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
